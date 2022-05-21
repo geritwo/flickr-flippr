133819f1a8f4c8ef148b7e87dc00794700102bd9
Gergo Vamosi <geritwo@gmail.com>
2022-05-21 16:56:27 +0200

    Refactor to functions

--- main.py
+++ main.py
@@ -17,88 +17,105 @@ try:
 except KeyError as e:
     exit("Error: FLICKR_API_KEY and/or FLICKR_API_SECRET not set in environment.")
 
-flickr = flickrapi.FlickrAPI(API_KEY, API_SECRET, format='parsed-json')
 
-print('Step 1: authenticate')
+def init_flickr(API_KEY, API_SECRET) -> object:
+    flickr = flickrapi.FlickrAPI(API_KEY, API_SECRET, format='parsed-json')
 
-# Only do this if we don't have a valid token already
-if not flickr.token_valid(perms='read'):
-    # Get a request token
-    flickr.get_request_token(oauth_callback='oob')
+    print('Step 1: authenticate')
 
-    # Open a browser at the authentication URL. Do this however
-    # you want, as long as the user visits that URL.
-    authorize_url = flickr.auth_url(perms='read')
-    webbrowser.open_new_tab(authorize_url)
+    # Only do this if we don't have a valid token already
+    if not flickr.token_valid(perms='read'):
+        # Get a request token
+        flickr.get_request_token(oauth_callback='oob')
 
-    # Get the verifier code from the user. Do this however you
-    # want, as long as the user gives the application the code.
-    verifier = str(input('Verifier code: '))
+        # Open a browser at the authentication URL. Do this however
+        # you want, as long as the user visits that URL.
+        authorize_url = flickr.auth_url(perms='read')
+        webbrowser.open_new_tab(authorize_url)
 
-    # Trade the request token for an access token
-    flickr.get_access_token(verifier)
+        # Get the verifier code from the user. Do this however you
+        # want, as long as the user gives the application the code.
+        verifier = str(input('Verifier code: '))
 
-print('Step 2: use Flickr')
+        # Trade the request token for an access token
+        flickr.get_access_token(verifier)
 
-# Demo functions
-photos = flickr.people.getPhotos(user_id=FLICKR_USER_ID, extras='tags,'
-                                                         'description,'
-                                                         'original_format,'
-                                                         'o_dims,'
-                                                         'date_taken,'
-                                                         'date_upload,'
-                                                         'geo')
+    return flickr
 
-pages_total = photos['photos']['pages']
-photos_list = photos['photos']['photo']
+
+def get_photos_from_flickr(flickr):
+    print('Step 2: use Flickr')
+
+    # Demo functions
+    photos = flickr.people.getPhotos(user_id=FLICKR_USER_ID, extras='tags,'
+                                                                    'description,'
+                                                                    'original_format,'
+                                                                    'o_dims,'
+                                                                    'date_taken,'
+                                                                    'date_upload,'
+                                                                    'geo')
+
+    return {
+        'TotalPages': photos['photos']['pages'],
+        'PhotosList': photos['photos']['photo']
+    }
+
 
 print('Step 3: use PSQL')
 conn = psycopg2.connect(host=DB_HOST, dbname=DB_NAME,
                         user=DB_USER, password=DB_PASSWORD,
                         port=5432)
 
-cur = conn.cursor()
 
-cur.execute("CREATE TABLE IF NOT EXISTS test (id serial PRIMARY KEY, data VARCHAR);")
-
-cur.execute(init_db)
-conn.commit()
-
-for photo in photos_list:
-    cur.execute("""
-                INSERT INTO photo_pool (
-                    title, description, 
-                    date_taken, date_upload,
-                    is_public, is_friend, is_family, 
-                    tags,
-                    latitude, longitude,
-                    original_format, o_width, o_height,
-                    secret, original_secret,
-                    server, farm, context_id)
-                VALUES (
-                    %(title)s, %(description)s, 
-                    %(date_taken)s, %(date_upload)s,
-                    %(is_public)s, %(is_friend)s, %(is_family)s, 
-                    %(tags)s,
-                    %(latitude)s, %(longitude)s,
-                    %(original_format)s, %(o_width)s, %(o_height)s,
-                    %(secret)s, %(original_secret)s,
-                    %(server)s, %(farm)s, %(context_id)s
-                ); 
-                """,
-                {
-                    "title": photo['title'], "description": photo['description']['_content'],
-                    "date_taken": photo['datetaken'], "date_upload": int(photo['dateupload']),
-                    "is_public": int(photo['ispublic']), "is_friend": int(photo['isfriend']),
-                    "is_family": int(photo['isfamily']),
-                    "tags": photo['tags'],
-                    "latitude": int(photo['latitude']), "longitude": int(photo['longitude']),
-                    "original_format": photo['originalformat'],
-                    "o_width": int(photo['o_width']), "o_height": int(photo['o_height']),
-                    "secret": photo['secret'], "original_secret": photo['originalsecret'],
-                    "server": photo['server'], "farm": int(photo['farm']), "context_id": int(photo['context'])
-                })
+def create_database(connection, database_name) -> None:
+    cur = connection.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS test (id serial PRIMARY KEY, data VARCHAR);")
+    cur.execute(init_db)
     conn.commit()
 
-cur.close()
-conn.close()
+
+def store_meta_in_db(connection, photos_list) -> None:
+    for photo in photos_list:
+        connection.cur.execute("""
+                    INSERT INTO photo_pool (
+                        title, description, 
+                        date_taken, date_upload,
+                        is_public, is_friend, is_family, 
+                        tags,
+                        latitude, longitude,
+                        original_format, o_width, o_height,
+                        secret, original_secret,
+                        server, farm, context_id)
+                    VALUES (
+                        %(title)s, %(description)s, 
+                        %(date_taken)s, %(date_upload)s,
+                        %(is_public)s, %(is_friend)s, %(is_family)s, 
+                        %(tags)s,
+                        %(latitude)s, %(longitude)s,
+                        %(original_format)s, %(o_width)s, %(o_height)s,
+                        %(secret)s, %(original_secret)s,
+                        %(server)s, %(farm)s, %(context_id)s
+                    ); 
+                    """,
+                    {
+                        "title": photo['title'], "description": photo['description']['_content'],
+                        "date_taken": photo['datetaken'], "date_upload": int(photo['dateupload']),
+                        "is_public": int(photo['ispublic']), "is_friend": int(photo['isfriend']),
+                        "is_family": int(photo['isfamily']),
+                        "tags": photo['tags'],
+                        "latitude": int(photo['latitude']), "longitude": int(photo['longitude']),
+                        "original_format": photo['originalformat'],
+                        "o_width": int(photo['o_width']), "o_height": int(photo['o_height']),
+                        "secret": photo['secret'], "original_secret": photo['originalsecret'],
+                        "server": photo['server'], "farm": int(photo['farm']), "context_id": int(photo['context'])
+                    })
+        connection.commit()
+
+    connection.cur.close()
+    connection.close()
+
+
+if __name__ == '__main__':
+    flickr_reader = init_flickr()
+    photos_meta = get_photos_from_flickr(flickr_reader)
+    #create_database()
